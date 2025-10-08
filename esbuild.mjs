420aed1cdb1896f707c9d7089ba3d360c537c37b
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-08 16:06:07 -0400

    Absolute cinema

--- esbuild.mjs
+++ esbuild.mjs
@@ -2,4 +2,20 @@
 import { azExtEsbuildConfigProd } from '@microsoft/vscode-azext-eng';
 import { build } from 'esbuild';
 
-await build(azExtEsbuildConfigProd);
+/** @type {import('esbuild').BuildOptions} */
+const config = {
+    ...azExtEsbuildConfigProd,
+    entryPoints: [
+        ...azExtEsbuildConfigProd.entryPoints,
+        {
+            in: './node_modules/dockerfile-language-server-nodejs/lib/server.js',
+            out: 'dockerfile-language-server-nodejs/lib/server',
+        },
+        {
+            in: './node_modules/@microsoft/compose-language-service/lib/server.js',
+            out: 'compose-language-service/lib/server',
+        },
+    ],
+};
+
+await build(config);
