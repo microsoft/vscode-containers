6e5e3c1fcd4f9807a5a123aa787e8655d016cde2
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-08 15:31:50 -0400

    No but like for real

---
+++ webpack.config.mjs
@@ -0,0 +1,42 @@
+// Many other configurations exist
+import { azExtWebpackConfigProd } from '@microsoft/vscode-azext-eng';
+
+const baseConfig = azExtWebpackConfigProd;
+
+/** @type {import('webpack').Configuration} */
+export default {
+    ...baseConfig,
+    entry: {
+        ...baseConfig.entry,
+        './dockerfile-language-server-nodejs/lib/server': './node_modules/dockerfile-language-server-nodejs/lib/server.js',
+        './compose-language-service/lib/server': './node_modules/@microsoft/compose-language-service/lib/server.js',
+    },
+    module: {
+        rules: [
+            ...baseConfig.module.rules,
+            {
+                // Unpack UMD module headers used in some modules since webpack doesn't handle them.
+                test: /dockerfile-language-service|vscode-languageserver-types/,
+                use: { loader: 'umd-compat-loader' }
+            },
+        ],
+    },
+    ignoreWarnings: [
+        {
+            // Ignore some warnings from handlebars in code that doesn't get used anyway
+            module: /node_modules\/handlebars\/lib\/index\.js/,
+            message: /require\.extensions/,
+        },
+        {
+            // Ignore a warning for a missing optional dependency of `ws` via `@microsoft/vscode-azext-azureappservice`
+            module: /node_modules\/ws/,
+            message: /Can't resolve 'bufferutil'/,
+        },
+        {
+            // Ignore another warning for a missing optional dependency of `ws` via `@microsoft/vscode-azext-azureappservice`
+            module: /node_modules\/ws/,
+            message: /Can't resolve 'utf-8-validate'/,
+        },
+        ...baseConfig.ignoreWarnings,
+    ],
+};
