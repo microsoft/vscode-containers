edb834a7f4396229d66ee85288496cb98bf2dd51
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-09 12:41:35 -0400

    eslint config improvements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,25 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export { azExtEslintRecommended as default } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
+import { azExtEslintRecommended } from '@microsoft/vscode-azext-eng/eslint'; // Other configurations exist
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    azExtEslintRecommended,
+    {
+        rules: {
+            '@typescript-eslint/no-restricted-imports': [
+                'error',
+                {
+                    patterns: [
+                        {
+                            group: ['@azure/*', 'handlebars', '@microsoft/vscode-azext-azureutils', '@microsoft/vscode-azext-azureappservice'],
+                            message: 'Please lazily import this package within the function that uses it to reduce extension activation time.',
+                            allowTypeImports: true,
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+]);
