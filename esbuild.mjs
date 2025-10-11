fb5257b1cf8878fd4e7f1c6e60fd9a4a5b23a772
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-10-11 19:39:22 -0400

    Upstream more of the esbuild config

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,21 +3,12 @@
  *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { azExtEsbuildConfigDebug, azExtEsbuildConfigDev, azExtEsbuildConfigProd } from '@microsoft/vscode-azext-eng/esbuild'; // Other configurations exist
-import * as esbuild from 'esbuild';
-import * as fs from 'fs/promises';
+import { autoBuildOrWatch, autoSelectConfig } from '@microsoft/vscode-azext-eng/esbuild';
 
-let baseConfig;
-if (process.env.DEBUG_ESBUILD) {
-    baseConfig = azExtEsbuildConfigDebug;
-} else if (process.argv.includes('--watch')) {
-    baseConfig = azExtEsbuildConfigDev;
-} else {
-    baseConfig = azExtEsbuildConfigProd;
-}
+const baseConfig = autoSelectConfig(false);
 
 /** @type {import('esbuild').BuildOptions} */
-const config = {
+const finalConfig = {
     ...baseConfig,
     entryPoints: [
         ...baseConfig.entryPoints,
@@ -32,17 +23,4 @@ const config = {
     ],
 };
 
-if (process.argv.includes('--watch')) {
-    const ctx = await esbuild.context(config);
-    process.on('SIGINT', () => {
-        console.log('Stopping esbuild watch');
-        ctx.dispose();
-    });
-    await ctx.watch();
-} else {
-    const result = await esbuild.build(config);
-
-    if (process.env.DEBUG_ESBUILD) {
-        await fs.writeFile('esbuild.meta.json', JSON.stringify(result.metafile));
-    }
-}
+await autoBuildOrWatch(finalConfig);
