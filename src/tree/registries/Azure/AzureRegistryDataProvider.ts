1182570bd33e5fda37bf8e7505721bf17bee6b88
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2025-11-13 15:25:09 -0500

    Start V6 auth integration

--- src/tree/registries/Azure/AzureRegistryDataProvider.ts
+++ src/tree/registries/Azure/AzureRegistryDataProvider.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import type { Registry as AcrRegistry, RegistryListCredentialsResult } from '@azure/arm-containerregistry';
-import { VSCodeAzureSubscriptionProvider, type AzureSubscription } from '@microsoft/vscode-azext-azureauth';
+import { getMetricsForTelemetry, VSCodeAzureSubscriptionProvider, type AzureSubscription } from '@microsoft/vscode-azext-azureauth';
 import { callWithTelemetryAndErrorHandling, createSubscriptionContext, type ISubscriptionActionContext } from '@microsoft/vscode-azext-utils';
 import { CommonRegistryItem, getContextValue, isRegistry, isRegistryRoot, isRepository, isTag, RegistryV2DataProvider, registryV2Request, V2Registry, V2RegistryItem, V2Repository, V2Tag } from '@microsoft/vscode-docker-registries';
 import * as vscode from 'vscode';
@@ -61,11 +61,21 @@ export class AzureRegistryDataProvider extends RegistryV2DataProvider implements
 
     public override async getChildren(element?: CommonRegistryItem | undefined): Promise<CommonRegistryItem[]> {
         if (isRegistryRoot(element)) {
-            if (!await this.subscriptionProvider.isSignedIn()) {
-                await this.subscriptionProvider.signIn();
-            }
+            //const msftAccount = (await this.subscriptionProvider.getAccounts()).find(a => a.label.toLowerCase().includes('microsoft.com'));
+            // TODO
+            const result = await this.subscriptionProvider.signIn(undefined, { promptIfNeeded: true });
+            ext.outputChannel.appendLine(`Sign-in for Microsoft account: ${result ? 'succeeded' : 'failed'}`);
 
-            const subscriptions = await this.subscriptionProvider.getSubscriptions();
+            // for (const account of await this.subscriptionProvider.getAccounts()) {
+            //     const accountSignInResult = await this.subscriptionProvider.signIn({ account: account, tenantId: undefined! }, { promptIfNeeded: false });
+            //     ext.outputChannel.appendLine(`Auto sign-in for account ${account.label}: ${accountSignInResult ? 'succeeded' : 'failed'}`);
+            //     for (const tenant of await this.subscriptionProvider.getTenantsForAccount(account)) {
+            //         const tenantSignInResult = await this.subscriptionProvider.signIn(tenant, { promptIfNeeded: false });
+            //         ext.outputChannel.appendLine(`Auto sign-in for account ${account.label} in tenant ${tenant.displayName}: ${tenantSignInResult ? 'succeeded' : 'failed'}`);
+            //     }
+            // }
+
+            const subscriptions = await this.subscriptionProvider.getAvailableSubscriptions();
             this.sendSubscriptionTelemetryIfNeeded();
 
             return subscriptions.map(sub => {
@@ -223,20 +233,19 @@ export class AzureRegistryDataProvider extends RegistryV2DataProvider implements
             context.telemetry.properties.isActivationEvent = 'true';
             context.errorHandling.suppressDisplay = true;
 
-            const subscriptions = await this.subscriptionProvider.getSubscriptions(false);
+            const {
+                totalAccounts,
+                totalTenants,
+                maybeTotalSubscriptions,
+                subscriptionIdList,
+            } = await getMetricsForTelemetry(this.subscriptionProvider);
 
-            const tenantSet = new Set<string>();
-            const subscriptionSet = new Set<string>();
-            subscriptions.forEach(sub => {
-                tenantSet.add(sub.tenantId);
-                subscriptionSet.add(sub.subscriptionId);
-            });
-
-            // Number of tenants and subscriptions really belong in Measurements but for backwards compatibility
+            // These counts really belong in Measurements but for backwards compatibility
             // they will be put into Properties instead.
-            context.telemetry.properties.numtenants = tenantSet.size.toString();
-            context.telemetry.properties.numsubscriptions = subscriptionSet.size.toString();
-            context.telemetry.properties.subscriptions = JSON.stringify(Array.from(subscriptionSet));
+            context.telemetry.properties.numaccounts = totalAccounts.toString();
+            context.telemetry.properties.numtenants = totalTenants.toString();
+            context.telemetry.properties.numsubscriptions = maybeTotalSubscriptions.toString();
+            context.telemetry.properties.subscriptions = subscriptionIdList;
         });
     }
 }
