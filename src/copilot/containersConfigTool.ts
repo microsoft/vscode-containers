6779c07bdfe76fd2d1195be7d60e1f30f967423c
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2026-01-20 11:06:46 -0500

    Some changes

--- src/copilot/containersConfigTool.ts
+++ src/copilot/containersConfigTool.ts
@@ -11,6 +11,8 @@ import { isComposeV2ableOrchestratorClient } from '../runtimes/clients/AutoConfi
 
 export class ContainersConfigTool implements AzExtLMTool<void> {
     public async invoke(context: IActionContext, options: vscode.LanguageModelToolInvocationOptions<void>, token: vscode.CancellationToken): Promise<vscode.LanguageModelToolResult> {
+        context.errorHandling.suppressDisplay = true;
+
         const containerClient = await ext.runtimeManager.getClient();
         const orchestratorClient = await ext.orchestratorManager.getClient();
         const envConfig = vscode.workspace.getConfiguration(configPrefix).get<Record<string, string>>('environment', {});
@@ -22,10 +24,16 @@ export class ContainersConfigTool implements AzExtLMTool<void> {
             orchestratorCommand = orchestratorClient.commandName;
         }
 
-        return new vscode.LanguageModelToolResult([
+        const results = [
             new vscode.LanguageModelTextPart(`Use the following base command for the container CLI: \`${containerClient.commandName}\``),
             new vscode.LanguageModelTextPart(`Use the following base command for the container orchestrator (compose) CLI: \`${orchestratorCommand}\``),
-            new vscode.LanguageModelTextPart(`Use the following environment variables when running container commands: \`\`\`json\n${JSON.stringify(envConfig)}\n\`\`\``)
-        ]);
+        ];
+
+        if (Object.keys(envConfig).length > 0) {
+            context.telemetry.properties.hasEnvConfig = 'true';
+            results.push(new vscode.LanguageModelTextPart(`The following environment variables will be set automatically for new terminals created by the agent: \`\`\`json\n${JSON.stringify(envConfig)}\n\`\`\``));
+        }
+
+        return new vscode.LanguageModelToolResult(results);
     }
 }
