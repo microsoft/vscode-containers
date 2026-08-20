987bb71a44bbe66869414378808443e2a841acf1
Zhibo Lin <147509942+LE0-Lin@users.noreply.github.com>
2026-08-20 21:54:23 +0800

    Add unpause action for paused containers

--- extensions/vscode-containers/src/commands/containers/unpauseContainer.ts
+++ extensions/vscode-containers/src/commands/containers/unpauseContainer.ts
@@ -1,6 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See LICENSE.md in the project root for license information.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
 import { IActionContext } from '@microsoft/vscode-azext-utils';
@@ -9,18 +9,18 @@ import { ext } from '../../extensionVariables';
 import { ContainerTreeItem } from '../../tree/containers/ContainerTreeItem';
 import { multiSelectNodes } from '../../utils/multiSelectNodes';
 
-export async function startContainer(context: IActionContext, node?: ContainerTreeItem, nodes?: ContainerTreeItem[]): Promise<void> {
+export async function unpauseContainer(context: IActionContext, node?: ContainerTreeItem, nodes?: ContainerTreeItem[]): Promise<void> {
     nodes = await multiSelectNodes(
-        { ...context, noItemFoundErrorMessage: vscode.l10n.t('No containers are available to start') },
+        { ...context, noItemFoundErrorMessage: vscode.l10n.t('No containers are available to unpause') },
         ext.containersTree,
-        /^(created|dead|exited|terminated)Container$/i,
+        /^pausedContainer$/i,
         node,
         nodes
     );
 
-    await vscode.window.withProgress({ location: vscode.ProgressLocation.Notification, title: vscode.l10n.t('Starting Container(s)...') }, async () => {
+    await vscode.window.withProgress({ location: vscode.ProgressLocation.Notification, title: vscode.l10n.t('Unpausing Container(s)...') }, async () => {
         await ext.runWithDefaults(client =>
-            client.startContainers({ container: nodes.map(n => n.containerId) })
+            client.unpauseContainers({ container: nodes.map(n => n.containerId) })
         );
     });
 }
